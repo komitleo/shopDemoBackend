e35ac8aad5d43b066613c718718d00fdf0f6ff0c
Komitleo-PC <jacek@komitleo.pl>
2022-11-03 23:31:35 +0100

    initial commit

---
+++ src/test/java/com/komitleo/shopdemo/ShopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komitleo.shopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
