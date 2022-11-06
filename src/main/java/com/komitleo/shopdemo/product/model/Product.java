d481bc2f7a14dad3928dc24de500e05c8c1744a3
Komitleo-PC <jacek@komitleo.pl>
2022-11-06 21:50:20 +0100

    .1: Auto stash before merge of "0.1" and "origin/0.1"

--- src/main/java/com/komitleo/shopdemo/product/model/Product.java
+++ src/main/java/com/komitleo/shopdemo/product/model/Product.java
@@ -2,11 +2,20 @@ package com.komitleo.shopdemo.product.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+<<<<<<< Updated upstream
+=======
+import lombok.Setter;
+
+>>>>>>> Stashed changes
 import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor
 public class Product {
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     private String name;
     private String category;
     private String description;
