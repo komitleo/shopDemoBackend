b247a56e44d0f88bea5232473ab863d72b475f40
Komitleo-PC <jacek@komitleo.pl>
2022-11-06 21:51:06 +0100

    check

--- src/main/java/com/komitleo/shopdemo/product/model/Product.java
+++ src/main/java/com/komitleo/shopdemo/product/model/Product.java
@@ -2,20 +2,13 @@ package com.komitleo.shopdemo.product.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-<<<<<<< Updated upstream
-=======
 import lombok.Setter;
-
->>>>>>> Stashed changes
 import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor
 public class Product {
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     private String name;
     private String category;
     private String description;
