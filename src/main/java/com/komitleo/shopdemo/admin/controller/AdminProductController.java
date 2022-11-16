63e1cc46c75f07388f562933a627efda6876f597
Komitleo-PC <jacek@komitleo.pl>
2022-11-16 15:11:41 +0100

    Add OPENAPI. Create Admin Product resources.

--- src/main/java/com/komitleo/shopdemo/admin/controller/AdminProductController.java
+++ src/main/java/com/komitleo/shopdemo/admin/controller/AdminProductController.java
@@ -1,22 +1,51 @@
 package com.komitleo.shopdemo.admin.controller;
 
+import com.komitleo.shopdemo.admin.controller.dto.AdminProductDto;
 import com.komitleo.shopdemo.admin.model.AdminProduct;
 import com.komitleo.shopdemo.admin.service.AdminProductService;
+import com.komitleo.shopdemo.product.model.Product;
 import com.komitleo.shopdemo.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminProductController {
 
-    private final AdminProductService AdminProductService;
+    public static final Long EMPTY_ID = null;
+    private final AdminProductService adminProductService;
 
     @GetMapping("/admin/products")
     public Page <AdminProduct> getProducts(Pageable pageable){
-        return AdminProductService.getProducts(pageable);
+        return adminProductService.getProducts(pageable);
     }
+    @GetMapping("/admin/products/{id}")
+    public AdminProduct getProduct(@PathVariable Long id){
+        return adminProductService.getProduct(id);
+    }
+
+    @PostMapping("/admin/products")
+    public AdminProduct createProduct(@RequestBody AdminProductDto adminProductDto) {
+        return adminProductService.createProduct(mapAdminProduct(adminProductDto, EMPTY_ID));
+    }
+        @PutMapping("/admin/products/{id}")
+        public AdminProduct updateProduct(@RequestBody AdminProductDto adminProductDto, @PathVariable Long id){
+            return adminProductService.updateProduct(mapAdminProduct(adminProductDto, id)
+            );
+
+    }
+
+    private AdminProduct mapAdminProduct(AdminProductDto adminProductDto, Long id) {
+        return AdminProduct.builder()
+                .id(id)
+                .name(adminProductDto.getName())
+                .description(adminProductDto.getDescription())
+                .category(adminProductDto.getCategory())
+                .price(adminProductDto.getPrice())
+                .currency(adminProductDto.getCurrency())
+                .build();
+    }
+
 }
--- src/main/java/com/komitleo/shopdemo/admin/model/AdminProduct.java
+++ src/main/java/com/komitleo/shopdemo/admin/model/AdminProduct.java
@@ -1,6 +1,9 @@
 package com.komitleo.shopdemo.admin.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -8,11 +11,14 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "product")
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminProduct {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
     private String name;
     private String category;
     private String description;
